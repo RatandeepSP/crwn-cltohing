16c02f50c92d22c43821b5bcec97eec7137c2526
Ratandeep <Ratandeep.pattarh2@gmail.com>
2021-01-24 17:23:19 +0530

    sign up integrated with firebase

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -11,8 +11,33 @@ const config = {
     appId: "1:415235993936:web:aa1544b26c067b52a47c55",
     measurementId: "G-D6PXYFE0XR"
   };
+  
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+      if(!userAuth) return;
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
+      const snapShot = await userRef.get();
+      if(!snapShot.exists){
+          const { displayName, email } = userAuth;
+          const createdAt = new Date();
 
-  firebase.initializeApp(config);
+          try{
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+          }
+          catch(error){
+            console.log('error creating user', error.message);
+          }
+      }
+      return userRef;
+  }
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+ }
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
